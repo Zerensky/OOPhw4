9d083a29ffb5717b5f8247877d540acd31ee21af
Zerensky <pure.evil97@mail.ru>
2022-12-28 18:21:17 +0600

    Вид щита Баклер

---
+++ Bakler.java
@@ -0,0 +1,11 @@
+public class Bakler extends Shield{
+    @Override
+    public int protectPoint() {
+        return 10;
+    }
+
+    @Override
+    public String toString() {
+        return "Щит типа Баклер" + super.toString();
+    }
+}
